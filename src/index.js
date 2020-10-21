a93eb4b4474c330a4c9c85288987848c2e1beb5b
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-10-21 11:52:23 -0400

    class game e class board added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Game(props) {
-    return <div>Hello World :c</div>
+class Board extends React.Component {
+    render() {
+        return <div>Board</div>;
+    }
+}
+
+// function é apenas pra renderização
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+
+                <div className="game-info">
+                    Informações do Game
+                </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
